0587225d3e7679b7485408618049484345de0c1b
Francis Wang <go717franciswang@hotmail.com>
2015-11-29 15:37:33 -0800

    Receive ws connection at /player_es

--- src/cardgames_app.erl
+++ src/cardgames_app.erl
@@ -5,6 +5,12 @@
 -export([stop/1]).
 
 start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+            {'_', [{"/player_ws", player_ws_handler, []}]}
+        ]),
+    {ok, _} = cowboy:start_http(my_http_listener, 100, [{port, 8080}],
+        [{env, [{dispatch, Dispatch}]}]
+    ),
 	cardgames_sup:start_link().
 
 stop(_State) ->
