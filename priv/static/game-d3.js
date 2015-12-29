2ef77ad0b4de5610d7a9d2614d078698039ea254
Francis Wang <go717franciswang@hotmail.com>
2015-12-28 22:57:31 -0800

    Switch to d3 to display table

--- priv/static/game-d3.js
+++ priv/static/game-d3.js
@@ -6,12 +6,23 @@ function draw(gameState) {
         player2nickname[user['player']] = user['nickname'];
     }
 
-    var width = 200;
+    var width = 300;
     var height = 200;
     var seatCount = gameState['seats'].length;
 
     var svg = d3.select('#container').selectAll('#game').data([gameState]);
-    svg.enter().append('svg').attr('id', 'game').attr('width', width).attr('height', height); 
+    var svgEnter = svg.enter().append('svg').attr('id', 'game').attr('width', width).attr('height', height); 
+    svgEnter.append('text').attr('class', 'cc').attr('x', width/2).attr('y', height*0.55).attr('text-anchor', 'middle');
+    svgEnter.append('text').attr('class', 'pots').attr('x', width/2).attr('y', height*0.45).attr('text-anchor', 'middle');
+
+    svg.select('text.cc').text(function(d) {
+        return 'CC: ' + cards2str(d['community_cards']);
+    });
+
+    svg.select('text.pots').text(function(d) {
+        return 'Pots: ' + d['pots'].map(function(p) { return p['money'] }).join(' ');
+    });
+
     var seat = svg.selectAll('g.d3seat').data(function(d) { 
         return d['seats'] 
     });
@@ -22,22 +33,54 @@ function draw(gameState) {
         var y = Math.sin(degree)*height*0.4+height/2;
         return 'translate('+x+','+y+')';
     });
-    seatEnter.append('text').attr('class', 'nickname');
-    seatEnter.append('text').attr('class', 'money').attr('y', 15);
+    seatEnter.append('text').attr('class', 'nickname').attr('text-anchor', 'middle');
+    seatEnter.append('text').attr('class', 'money').attr('y', 15).attr('dx', -20).attr('text-anchor', 'middle');
+    seatEnter.append('text').attr('class', 'bet').attr('y', 15).attr('dx', 20).attr('text-anchor', 'middle');
+    seatEnter.append('text').attr('class', 'cards').attr('y', 30).attr('text-anchor', 'middle');
     seat.select('.nickname').text(function(d) { 
         return d['player'] ? player2nickname[d['player']] : '-';
     });
     seat.select('.money').text(function(d) { 
-        return d['money'] ? d['money'] : '';
+        return d['player'] ? d['money'] : '';
+    });
+    seat.select('.bet').text(function(d) { 
+        return d['player'] ? Math.min(d['money'],d['bet']) : '';
+    });
+    seat.select('.cards').text(function(d) { 
+        if (d['player']) console.log('cards', cards2str(d['cards']));
+        return d['player'] ? cards2str(d['cards']) : '';
     });
 
-    var cards = seatEnter.append('g').attr('class', 'cards');
-    var card = cards.selectAll('text.card').data(function(d) { 
-        return d['cards'] ? d['cards'] : [];
-    });
-    card.enter().append('text').attr('class', 'card');
-    card.text(function(d) { return d['rank']+' '+d['suit'] });
-    card.exit().remove();
 }
 
+var suit2str = {
+    'spade': '\u2660',
+    'heart': '\u2665',
+    'diamond': '\u2666',
+    'club': '\u2663'
+};
+var rank2str = {
+    'two': '2',
+    'three': '3',
+    'four': '4',
+    'five': '5',
+    'six': '6',
+    'seven': '7',
+    'eight': '8',
+    'nine': '9',
+    'ten': 'T',
+    'jack': 'J',
+    'queen': 'Q',
+    'king': 'K',
+    'ace': 'A'
+};
+
+function card2str(card) {
+    if (card == 'unknown') return '?';
+    return rank2str[card['rank']] + suit2str[card['suit']];
+}
+
+function cards2str(cards) {
+    return cards.map(card2str).join(' ');
+}
 
